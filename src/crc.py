9b957cae934911c5a0199c6c6f2fbd1a1e5b3a0a
Jonathan Steininger <jonathan.steininger@icloud.com>
2023-10-12 20:04:15 +1300

    cleaned up CRC module

--- src/crc.py
+++ src/crc.py
@@ -1,13 +1,15 @@
 from bitarray import bitarray
+from enum import Enum
 
 
 def CRC32(message: bytes) -> bytes:
     poli = getPolinomial32()
+    # initiates the CRC bit array that will get manipulated by bitwise operations.
     crc = bitarray('1' * 32)
+    # loops through each byte in the message
     for byte in message:
         byteBits = bitarray()
-        byteByte = byte.to_bytes(1, 'big')
-        byteBits.frombytes(byteByte)
+        byteBits.frombytes(byte.to_bytes(1, 'big'))
         crc[:8] ^= byteBits
         for void in range(8):
             if crc[-1]:
@@ -18,48 +20,19 @@ def CRC32(message: bytes) -> bytes:
     return crc.tobytes()
 
 
-def CRC(message: bytes) -> bytes:
-    messageBits = bitarray(endian='big')
-    messageBits.frombytes(message)
-    polinomial = getPolinomial32()
-    outBits = runCRCOperation(messageBits, polinomial)
-    desiredLen = 8 * 4
-    outBits = padBits(outBits, desiredLen)
-    outBits = ~outBits
-    return outBits.tobytes()
-
-
-def padBits(bitsIn: bitarray, length: int) -> bitarray:
-    lenBitsIn = len(bitsIn)
-    if (lenBitsIn >= length):
-        return bitsIn
-    diffLength = length - lenBitsIn
-    outBits = bitarray(diffLength)
-    outBits.setall(0)
-    outBits.extend(bitsIn)
-    return outBits
+class Polinomial32():
+    Normal = 0x04C11DB7
+    Reversed = 0xEDB88320
+    reciprocal = 0xDB710641
+    reversedReciprocal = 0x82608EDB
 
 
 def getPolinomial32() -> bitarray:
-    normal = 0x04C11DB7
-    reversed = 0xEDB88320
-    reciprocal = 0xDB710641
-    reversedReciprocal = 0x82608EDB
-    polinomialBytes = reversed.to_bytes(4, 'big')
+    poliNum = Polinomial32().Reversed
+    polinomialBytes = poliNum.to_bytes(4, 'big')
     polinomialBits = bitarray(endian='big')
     polinomialBits.frombytes(polinomialBytes)
     return polinomialBits
     startingIndex = polinomialBits.find(1)
     polinomialBits = polinomialBits[startingIndex:]
     return polinomialBits
-
-
-def runCRCOperation(message: bitarray, poli: bitarray) -> bitarray:
-    lengthMessage = len(message)
-    lengthPoli = len(poli)
-    message.extend('0' * (lengthPoli - 1))
-    for pos in range(lengthMessage):
-        if (message[pos] == 1):
-            message[pos:pos + lengthPoli] ^= poli
-    finalBits = message[lengthMessage:]
-    return finalBits
